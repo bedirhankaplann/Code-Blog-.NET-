528ded9570a703af85b9b9bae685c5ee2621a37b
Bedirhan Kaplan <bedirhankaplan@outlook.com.tr>
2020-03-01 23:41:34 +0300

    Proje dosyası ekle.

---
+++ Code Blog/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Code_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [Route("post")]
+        public ActionResult Post()
+        {
+            return View();
+        }
+    }
+}
